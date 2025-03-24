6edde922fbda82bfb7ac61eadf4120273446b506
drewby <117329972+druwby@users.noreply.github.com>
2025-03-23 17:34:26 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,19 @@
+RED = '\033[31m'
+GREEN = '\033[32m'
+YELLOW = '\0 প্রেস[33m'
+BLUE = '\033[34m'
+MAGENTA = '\033[35m'
+CYAN = '\033[36m'
+WHITE = '\033[37m'
+RESET = '\033[0m'
+
+print(RED + "This is red text" + RESET)
+print(GREEN + "This is green text" + RESET)
+print(YELLOW + "This is yellow text" + RESET)
+print(BLUE + "This is blue text" + RESET)
+print(MAGENTA + "This is magenta text" + RESET)
+print(CYAN + "This is cyan text" + RESET)
+print(WHITE + "This is white text" + RESET)
+
+class Square:
+    
