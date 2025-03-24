44174f9b374a32ce8a427f4e872d26a1bb8f36bb
drewby <117329972+druwby@users.noreply.github.com>
2025-03-23 19:39:21 -0700

    working bfs visualizer with test included in main

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import queue
+import time
+import networkx as nx
+import matplotlib.pyplot as plot
+
 
 #BFS search logic
 
@@ -16,3 +20,30 @@ def order_bfs(graph, start_node):
             for node in graph[vertex]:
                 if node not in visited:
                     q.put(node)
+    return order
+
+
+def search_visualizer(order, title, G, pos):
+    plot.figure()
+    plot.title(title)
+    for i, node in enumerate(order, start=1):
+        plot.clf()
+        plot.title(title)
+        nx.draw(G, pos, with_labels=True, node_color=['b' if n == node else ('r' if G.degree(n) == 0 else 'g') for n in G.nodes()])
+        plot.draw()
+        plot.pause(2)
+    plot.show()
+    time.sleep(2)
+
+G = nx.grid_2d_graph(5,5)
+pos = pos = dict((n, n) for n in G.nodes())
+donotvisit = []
+for each in G.edges((2,2)):
+    donotvisit.append(each)
+for each in G.edges((0,2)):
+    donotvisit.append(each)
+#donotvisit = G.edges(2,2)
+#donotvisit.extend(G.edges(2,1))
+G.remove_edges_from(donotvisit)
+    
+search_visualizer(order_bfs(G, (1,1)), 'BFS', G, pos)
